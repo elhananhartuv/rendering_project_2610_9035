bd895210f2c29663b537c7b030c281ac9d833822
אלחנן הרטוב <elchananhrtuv@gmail.com>
2020-05-18 18:26:49 +0300

    r7 with bonuse r6

--- src/unittests/LightsTests.java
+++ src/unittests/LightsTests.java
@@ -85,7 +85,7 @@ public class LightsTests {
 	}
 
 	/**
-	 * Produce a picture of a sphere lighted by a spot light
+	 * Produce a picture of a sphere lighted by a narrow spot light
 	 */
 	@Test
 	public void sphereSpotBonus() {
@@ -99,7 +99,7 @@ public class LightsTests {
 				new Sphere(new Point3D(0, 0, 50), 50, new Color(java.awt.Color.BLUE), new Material(0.5, 0.5, 100)));
 
 		scene.addLights(new SpotLight(new Vector(1, -1, 2), new Color(500, 300, 0), new Point3D(-50, 50, -50), 1,
-				0.00001, 0.00000001,4));
+				0.00001, 0.00000001, 4));
 
 		ImageWriter imageWriter = new ImageWriter("sphereSpotBonus", 150, 150, 500, 500);
 		Render render = new Render(imageWriter, scene);
@@ -107,6 +107,7 @@ public class LightsTests {
 		render.renderImage();
 		render.writeToImage();
 	}
+
 	/**
 	 * Produce a picture of a two triangles lighted by a directional light
 	 */
@@ -185,8 +186,9 @@ public class LightsTests {
 		render.renderImage();
 		render.writeToImage();
 	}
+
 	/**
-	 * Produce a picture of a two triangles lighted by a spot light
+	 * Produce a picture of a two triangles lighted by a narrow spot light
 	 */
 	@Test
 	public void trianglesSpotBonus() {
@@ -203,7 +205,7 @@ public class LightsTests {
 						new Point3D(-70, -70, 50), new Point3D(75, -75, 150)));
 
 		scene.addLights(new SpotLight(new Vector(-2, 2, 1), new Color(500, 250, 250), new Point3D(10, 10, 130), 1,
-				0.0001, 0.000005,4));
+				0.0001, 0.000005, 4));
 
 		ImageWriter imageWriter = new ImageWriter("trianglesSpotBonus", 200, 200, 500, 500);
 		Render render = new Render(imageWriter, scene);
@@ -211,6 +213,7 @@ public class LightsTests {
 		render.renderImage();
 		render.writeToImage();
 	}
+
 	/**
 	 * Produce a picture of a sphere lighted by all lights
 	 */
@@ -224,7 +227,7 @@ public class LightsTests {
 		scene.addGeometries(
 				new Sphere(new Point3D(0, 0, 50), 50, new Color(java.awt.Color.BLACK), new Material(0.5, 0.5, 100)));
 		scene.addLights(new DirectionalLight(new Color(400, 300, 0), new Vector(1, -1, 1)),
-				new PointLight(new Color(500, 250, 250), new Point3D(55,55, 100), 1, 0.0005, 0.0005),
+				new PointLight(new Color(500, 250, 250), new Point3D(55, 55, 100), 1, 0.0005, 0.0005),
 				new SpotLight(new Vector(-2, 2, 1), new Color(500, 250, 250), new Point3D(10, 10, 130), 1, 0, 0));
 
 		ImageWriter imageWriter = new ImageWriter("sphereAllLights", 150, 150, 500, 500);
