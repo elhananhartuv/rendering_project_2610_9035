8289369a0a4acda7355b0382ca41d41125cb3bf6
אלחנן הרטוב <elchananhrtuv@gmail.com>
2020-05-26 17:19:10 +0300

    07

--- src/unittests/CreateImage.java
+++ src/unittests/CreateImage.java
@@ -10,7 +10,6 @@ import geometries.Cylinder;
 import geometries.Plane;
 import geometries.Sphere;
 import geometries.Triangle;
-import geometries.Tube;
 import primitives.Color;
 import primitives.Material;
 import primitives.Point3D;
@@ -31,14 +30,14 @@ public class CreateImage {
 		scene.setAmbientLight(new AmbientLight(new Color(132, 124, 65), 0));
 
 		scene.addGeometries(
-				new Triangle(new Color(java.awt.Color.BLUE), new Point3D(0, -20, 0), new Point3D(20, 10, 0),
+				new Triangle(new Color(java.awt.Color.black), new Point3D(0, -20, 0), new Point3D(20, 10, 0),
 						new Point3D(-20, 10, 0)),
-				new Triangle(new Color(java.awt.Color.BLUE), new Point3D(0, 20, 0), new Point3D(20, -10, 0),
+				new Triangle(new Color(java.awt.Color.black), new Point3D(0, 20, 0), new Point3D(20, -10, 0),
 						new Point3D(-20, -10, 0)),
 				new Sphere(new Point3D(0, 0, 0), 50, new Color(java.awt.Color.PINK),
 						new Material(0.4, 0.3, 100, 0.3, 0)));
 
-		scene.addLights(new DirectionalLight(new Color(500, 300, 0), new Vector(1, -1, 1)));
+		scene.addLights(new DirectionalLight(new Color(300, 500, 0), new Vector(1, -1, 1)));
 
 		ImageWriter imageWriter = new ImageWriter("createImageTest", 150, 150, 500, 500);
 		Render render = new Render(imageWriter, scene);
@@ -76,10 +75,10 @@ public class CreateImage {
 				new Cylinder(new Ray(new Point3D(0, 50, 0), new Vector(50, -75, 5)), 2, 100,
 						new Color(java.awt.Color.BLUE), new Material(0.4, 0.3, 100, 0, 0)),
 				new Cylinder(new Ray(new Point3D(0, -150, 0), new Vector(0, 1, 0)), 2, 50,
-						new Color(java.awt.Color.BLUE), new Material(0.4, 0.3, 100, 0, 0)),
+						new Color(java.awt.Color.WHITE), new Material(0.4, 0.3, 100, 0, 0)),
 				new Sphere(new Point3D(0, -110, 0), 2, new Color(127, 127, 127), new Material(0.4, 0.3, 100, 0, 0)),
-				new Sphere(new Point3D(0, -100, 0), 5, new Color(127, 127, 127), new Material(0.4, 0.3, 100, 0.1, 0)),
-				new Sphere(new Point3D(0, -100, 0), 9, new Color(127, 127, 127), new Material(0.4, 0.3, 100,0.3, 0)),
+				new Sphere(new Point3D(0, -100, 0), 5, new Color(127, 127, 127), new Material(0.4, 0.3, 100, 1, 0)),
+				new Sphere(new Point3D(0, -100, 0), 9, new Color(127, 127, 127), new Material(0.4, 0.3, 100, 0.3, 0)),
 
 				new Triangle(new Color(116, 122, 99), new Material(0.8, 0.2, 300, 0, 0.3), new Point3D(-150, 150, 150),
 						new Point3D(-70, -70, 50), new Point3D(75, -75, 150)),
@@ -87,7 +86,7 @@ public class CreateImage {
 						new Point3D(150, 150, 150), new Point3D(75, -75, 150)));
 
 		scene.addLights(new DirectionalLight(new Color(48, 170, 176), new Vector(0, -1, 2)),
-				new PointLight(new Color(48, 170, 176), new Point3D(0, -100, 0), 1, 0,0));
+				new PointLight(new Color(103, 110, 13), new Point3D(0, -100, 0), 1, 0, 0));
 
 		ImageWriter imageWriter = new ImageWriter("magenDavid", 250, 250, 500, 500);
 		Render render = new Render(imageWriter, scene);
